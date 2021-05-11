cc9e19d46d78f645331dbf304b905b8cfc573fbd
Valentina Rua C <40582270+valerc123@users.noreply.github.com>
2021-05-11 00:50:51 -0500

    ui

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
@@ -10,21 +11,44 @@ public class PlayerController : MonoBehaviour
     private int score = 0;
     public int health = 5;
     private Vector3 move;
+    public Text scoreText;
+    public Text healthText;
+    public Text winLostText;
+    public GameObject winLostImage;
     void Start()
     {
+        winLostImage = GameObject.Find("WinLoseBG");
+        winLostImage.SetActive(false);
         rb = GetComponent<Rigidbody>();
-        speed = 20f;
+        speed = 10f;
+    }
+    void SetScoreText(){
+        scoreText.text = "Score: " + score.ToString();
+    }
+    void SetHealthText(){
+        healthText.text = "Health: " + health.ToString();
     }
     void Update(){
         move =  new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
         
         rb.AddForce(move * speed);
         if(health == 0){
-            Debug.Log("Game Over!");
-            SceneManager.LoadScene("maze");
             score = 0;
             health = 5;
+            winLostText.text = "Game Over!";
+            winLostImage.SetActive(true);
+            winLostImage.GetComponent<Image>().color = new Color(255,0,0,200);
+            winLostText.color = new Color(255,255,255);
+            StartCoroutine(LoadScene(3));
         }
+
+        if(Input.GetKey(KeyCode.Escape)){
+            SceneManager.LoadScene("menu");
+        }
+    }
+    IEnumerator LoadScene(float seconds){
+       yield return new WaitForSeconds(seconds);
+        SceneManager.LoadScene("maze");
     }
     public void FixedUpdate(){
         if(Input.GetKey(KeyCode.A)){
@@ -44,14 +68,21 @@ public class PlayerController : MonoBehaviour
     void OnTriggerEnter(Collider other){
         if (other.tag == "Pickup"){
             score++;
-            Debug.Log("Score: " + score);
+            SetScoreText();
+            //Debug.Log("Score: " + score);
         }
         if (other.tag == "Trap"){
             health--;
-            Debug.Log("Health: " + health);
+            SetHealthText();
+           // Debug.Log("Health: " + health);
         }
         if(other.tag == "Goal"){
-            Debug.Log("You win!");
+            StartCoroutine(LoadScene(3));
+            winLostText.text = "You win!";
+            winLostImage.SetActive(true);
+            winLostImage.GetComponent<Image>().color = new Color(0,255,0,200);
+            winLostText.color = new Color(0,0,0);
+            //Debug.Log("You win!");
         }
     }
 }
